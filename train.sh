905def9d5588a5c304db2fa4bbe510b937c906ef
Amirhossein Dadashzadeh <31938815+Plrbear@users.noreply.github.com>
2018-10-02 09:01:05 +0330

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python train.py --batch_size 2 --epochs 200 --lr 0.001 \
-                --ad1 '/home/amir/epi/fold2/' \
+                --ad1 '/home/amir/dataset1/fold1/' \
                 --ad2 'train' \
                 --ad3 'validation' \
                 --ad5 'mask' \
